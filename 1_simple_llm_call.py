ff4f8f7f1d1b997a3929c2ca25176e0d8ffa4011
Shubham8831 <shubuamsingh2002@gmail.com>
2025-08-20 13:41:01 +0530

    tracing simple llm

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
+import os 
 load_dotenv()
-
+key = os.getenv("GROQ_API_KEY")
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatOpenAI()
+model = ChatGroq(model="llama-3.3-70b-versatile", api_key=key)
 parser = StrOutputParser()
 
 # Chain: prompt → model → parser
--- 2_sequential_chain.py
+++ 2_sequential_chain.py
@@ -1,9 +1,19 @@
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os 
+
+# so ideally each project in langsmith will have diff names so to change them from here : 
+os.environ["LANGCHAIN_PROJECT"] = "sequential app" # this will over write the .env one 
+#(setting different project name )
 
 load_dotenv()
+key = os.getenv("GROQ_API_KEY")
+
+model1 = ChatGroq(model="llama-3.3-70b-versatile", api_key=key, temperature=0.7)
+model2 = ChatGroq(model="llama-3.3-70b-versatile", api_key=key, temperature=0.4)
+
 
 prompt1 = PromptTemplate(
     template='Generate a detailed report on {topic}',
@@ -15,12 +25,17 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-model = ChatOpenAI()
-
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model1 | parser | prompt2 | model2 | parser
 
-result = chain.invoke({'topic': 'Unemployment in India'})
+# we can also add/set tags and metadata here for our trace 
+config = {
+    "run_name": "sequiential chain", # to give your own name to the trace or run
+    'tags':{'llm app', "report generation", "summarization"},
+    'meta_data':{'model':"llama-3.3-70b-versatile", 'parser':"StrOutputParser"}
+}
+
+result = chain.invoke({'topic': 'Unemployment in India'}, config=config)
 
 print(result)
